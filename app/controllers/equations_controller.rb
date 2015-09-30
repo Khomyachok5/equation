b11309b2045be6af7432d93dbbbac8a51d57daa3
Vyacheslav Zenkevich <vyacheslav.zenkevich@teknavo.com>
2015-09-30 15:04:14 +0300

    Found the vertext of the parabola

--- app/controllers/equations_controller.rb
+++ app/controllers/equations_controller.rb
@@ -11,14 +11,15 @@ class EquationsController < ApplicationController
     if discriminant < 0
       @answer =  "The discriminant of the equation is negative, there are no roots"
     else
+      @vertex = find_vertex(a, b, c)
       case
         when discriminant > 0
           x1 = (-b + sqrt(discriminant)) / 2*a
           x2 = (-b - sqrt(discriminant)) / 2*a
-          @answer =  "THE DISCRIMINANT OF THE EQUATION IS #{discriminant} and the values entered are #{a}, #{b}, #{c}. The roots of the equation are #{x1} and #{x2}"
+          @answer =  "THE DISCRIMINANT OF THE EQUATION IS #{discriminant} and the values entered are #{a}, #{b}, #{c}. The roots of the equation are #{x1} and #{x2}. The vertex of the parabola should be at the following co-ordinates: X axis at #{@vertex[0]}, Y axis at #{@vertex[1]}"
         when discriminant == 0
           x1 = (-b + sqrt(discriminant)) / 2*a
-          @answer =  "THE DISCRIMINANT OF THE EQUATION IS #{discriminant} and the values entered are #{a}, #{b}, #{c}. The root of the equation is #{x1}"
+          @answer =  "THE DISCRIMINANT OF THE EQUATION IS #{discriminant} and the values entered are #{a}, #{b}, #{c}. The root of the equation is #{x1}. The vertex of the parabola should be at the following co-ordinates: X axis at #{@vertex[0]}, Y axis at #{@vertex[1]}"
       end
     end
   end
@@ -26,4 +27,15 @@ class EquationsController < ApplicationController
   def find_discriminant(a, b, c)
     discriminant=(b*b)-(4*a*c)
   end
+
+  def find_vertex(a, b, c)
+    h = -b/(2*a)
+    k = a*(h*h) + b*h + c
+    [h, k]
+  end
+
+  def additional_points(a, b, c)
+    
+  end
+
 end
