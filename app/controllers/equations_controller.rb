3f8c254f0a7e7866400346e9348c4860126126e5
Vyacheslav Zenkevich <vyacheslav.zenkevich@teknavo.com>
2015-09-30 13:45:41 +0300

    Quadratic equation is now correctly solved, but not yet graphed

--- app/controllers/equations_controller.rb
+++ app/controllers/equations_controller.rb
@@ -1,13 +1,26 @@
 class EquationsController < ApplicationController
+  include Math
   def main_page
   end
 
   def calculate
-    a = params[:a_value]
-    b = params[:b_value]
-    c = params[:c_value]
-    discriminant = find_discriminant(a.to_i, b.to_i, c.to_i)
-    render text: "THE DISCRIMINANT OF THE EQUATION IS #{discriminant} and the values entered are #{a}, #{b}, #{c}"
+    a = params[:a_value].to_i
+    b = params[:b_value].to_i
+    c = params[:c_value].to_i
+    discriminant = find_discriminant(a, b, c)
+    if discriminant < 0
+      @answer =  "The discriminant of the equation is negative, there are no roots"
+    else
+      case
+        when discriminant > 0
+          x1 = (-b + sqrt(discriminant)) / 2*a
+          x2 = (-b - sqrt(discriminant)) / 2*a
+          @answer =  "THE DISCRIMINANT OF THE EQUATION IS #{discriminant} and the values entered are #{a}, #{b}, #{c}. The roots of the equation are #{x1} and #{x2}"
+        when discriminant == 0
+          x1 = (-b + sqrt(discriminant)) / 2*a
+          @answer =  "THE DISCRIMINANT OF THE EQUATION IS #{discriminant} and the values entered are #{a}, #{b}, #{c}. The roots of the equation are #{x1}"
+      end
+    end
   end
 
   def find_discriminant(a, b, c)
