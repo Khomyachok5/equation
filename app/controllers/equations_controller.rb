4e195b001c4f7c2381b54ca150cd36b5d1dd262d
Vyacheslav Zenkevich <vyacheslav.zenkevich@teknavo.com>
2015-09-30 12:43:25 +0300

    Added conversions from string to int

--- app/controllers/equations_controller.rb
+++ app/controllers/equations_controller.rb
@@ -6,11 +6,11 @@ class EquationsController < ApplicationController
     a = params[:a_value]
     b = params[:b_value]
     c = params[:c_value]
-    discriminant = find_discriminant(a, b, c)
-    #render text: "THE DISCRIMINANT OF THE EQUATION IS #{discriminant} and the values entered are #{a}, #{b}, #{c}"
+    discriminant = find_discriminant(a.to_i, b.to_i, c.to_i)
+    render text: "THE DISCRIMINANT OF THE EQUATION IS #{discriminant} and the values entered are #{a}, #{b}, #{c}"
   end
 
   def find_discriminant(a, b, c)
-    discriminant=(b*b)−(4*a*c)
+    discriminant=(b*b)-(4*a*c)
   end
 end
