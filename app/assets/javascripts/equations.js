0689d89c50f909e9e040186458c0d24438214c5e
Vyacheslav Zenkevich <vyacheslav.zenkevich@teknavo.com>
2015-09-30 23:24:37 +0300

    Fixing Javascript function

--- app/assets/javascripts/equations.js
+++ app/assets/javascripts/equations.js
@@ -1,5 +1,7 @@
 $(function() {
   console.log( "ready!" );
+  var index;
+  var a =  Array.prototype.slice.call($('.element_name'), 0)
   var chart = c3.generate({
       bindto: '#chart',
       data: {
@@ -7,24 +9,9 @@ $(function() {
         y: 'y',
         columns: [
           ['x', $('#vertex_x').data('x'),
-           $('#element_name_0').data('x'),
-           $('#element_name_1').data('x'), 
-           $('#element_name_2').data('x'), 
-           $('#element_name_3').data('x'), 
-           $('#element_name_4').data('x'),
-           $('#element_name_5').data('x'),
-           $('#element_name_6').data('x'),
-           $('#element_name_7').data('x'),
-           $('#element_name_8').data('x'),
-           $('#element_name_9').data('x'),
-           $('#element_name_10').data('x'),
-           $('#element_name_11').data('x'),
-           $('#element_name_12').data('x'),
-           $('#element_name_13').data('x'),
-           $('#element_name_14').data('x'),
-           $('#element_name_15').data('x'),
-           $('#element_name_16').data('x'),
-           $('#element_name_17').data('x')],
+            for (index = 0; index < a.length; ++index) {
+              a[index].data('x');
+            } ],
           ['y', $('#vertex_y').data('y'),
             $('#element_y_name_0').data('y'), 
             $('#element_y_name_1').data('y'),
