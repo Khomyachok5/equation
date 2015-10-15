84253935485e7194051f7672bc39c5be14cead90
Vyacheslav Zenkevich <vyacheslav.zenkevich@teknavo.com>
2015-10-15 16:33:04 +0300

    Minor fixes

--- app/assets/javascripts/equations.js
+++ app/assets/javascripts/equations.js
@@ -2,32 +2,17 @@ $(function() {
   console.log( "ready!" );
 
   var parent_array = $('#charts_placeholder').data('parabolas');
-  // alert(parent_array.length);
   var all_parabolas = []
-
 
   for (index = 0; index < parent_array.length; ++index) {
     var x_points = [];
     var y_points = [];
-
-    //x_points.push('data_x');
-    //y_points.push('data_y');
-
-
-    //Object.keys(parent_array[index]).forEach(function(entry) { 
-      //x_points.push(entry)
-    // } );
-
-    //alert(x_points);
 
     parent_array[index].forEach(function(entry) {
       
       for (var key in entry) {
         x_points.push(Number(Math.round(parseFloat(key)+'e2') +'e-2'));
         y_points.push(Number(Math.round(parseFloat(entry[key])+'e2') +'e-2'));
-
-        //y_points.push(parseFloat(entry[key]))
-        //console.log(x_points);
       }
     });
 
@@ -37,64 +22,17 @@ $(function() {
     all_parabolas[index].push(copy_x_points = x_points.slice(0));
     all_parabolas[index].push(copy_y_points = y_points.slice(0));
 
-    //Number(Math.round(1.005+'e2')+'e-2');
-
-    //alert(y_points);
-
-    /*Object.values(parent_array[index]).forEach(function(entry) { 
-      y_points.push(entry)
-    } ); */
-
-    //alert(y_points);
     drawChart(index, x_points, y_points);
-
-    
   }
     
-  //console.log(all_parabolas)
   drawMasterChart(all_parabolas);
-  //alert(x_points);
-
-
-  /* for (index = 0; index < parent_array.length; ++index) {
-    x_points.push(Object.keys(parent_array[index]));
-  } */
-  
-
-  /*
-  var x_points = [];
-  var y_points = [];
-  x_points.push('data_x', $('#vertex_x').data('x'))
-  y_points.push('data_y', $('#vertex_y').data('y'))
-  var index;
-  var a =  Array.prototype.slice.call($('.element_name'), 0)
-  for (index = 0; index < a.length; ++index) {
-    x_points.push($(a[index]).data('x'));
-  }
-  var b =  Array.prototype.slice.call($('.element_y'), 0)
-  for (index = 0; index < b.length; ++index) {
-    y_points.push($(b[index]).data('y'));
-  }
-
-  */
 });
 
 
-function drawChart(index, x_points, y_points) { /* chart = */ 
+function drawChart(index, x_points, y_points) {
   x_points.unshift('data_x');
   y_points.unshift('parabola');
   y_points.push(0)
-  //console.log("x_points array values")
-  //console.log(x_points);
-  //console.log("y_points array values")
-  //console.log(y_points)
-
-  //axis_x = [];
-  //axis_y = [];
-  //for (var i = -20; i <= 20; i++) {
-    //axis_x.push(i);
-    //axis_y.push(i);
-  //}
 
   chart = c3.generate({
 
@@ -106,7 +44,7 @@ function drawChart(index, x_points, y_points) { /* chart = */
             label: {
               text: 'Y axis',
               position: 'outer-middle' },
-            //center: y_points[1],         
+         
             show: true,
             tick: {
                fit: true,
@@ -119,33 +57,19 @@ function drawChart(index, x_points, y_points) { /* chart = */
            },
 
         x: {
-            //type: 'category',
+
             show: true,
             label: {
                 text: 'X axis',
                 position: 'outer-center' },
             tick: {
-              //culling: {
-                //max: 4 // the number of tick texts will be adjusted to less than this value
-              //},
               multiline: false,
-              //values: x_points,
               },
-
             }
       },
 
       data: {
-
-        //xs: {
-            //'data_y': 'data_x'
-        //},
-
         x: 'data_x',
-        //y: 'data_y',
-        //parabola: 'parabola',
-        //x_data: 'data_x',
-        //y_data: 'data_y',
 
         columns: [
           x_points,
@@ -180,31 +104,11 @@ function drawMasterChart(all_parabolas) {
   for (index = 0; index < all_parabolas.length; ++index) {
     (all_parabolas[index])[0].unshift('x_points_'.concat(index));
     (all_parabolas[index])[1].unshift('y_points_'.concat(index));
-
-    //console.log("THE MODIFIED INDEX IS")
-    //console.log((all_parabolas[index])[0])
   };
 
-  //data_values_deps = (for (index = 0; index < all_parabolas.length; ++index) {
-    //'data_y'.concat(index): 'data_x'.concat(index); })
+  var a = {}
 
-  //console.log(data_values_deps)
-
-  var a = {
-          //'y_points_0': 'x_points_0',
-          //'y_points_1': 'x_points_1',
-          //'y_points_2': 'x_points_2',
-          //if (typeof(all_parabolas[3]) != "undefined") {'y_points_3': 'x_points_3'}
-        }
-
-    var b = [
-          //(all_parabolas[0])[0],
-          //(all_parabolas[0])[1],
-          //(all_parabolas[1])[0], //x_points_1,
-          //(all_parabolas[1])[1], //y_points_1,
-          //(all_parabolas[2])[0], //x_points_2,
-          //(all_parabolas[2])[1]  //y_points_2
-        ]
+  var b = []
 
   for (index = 0; index < all_parabolas.length; ++index) { 
     a['y_points_'.concat(index)] = 'x_points_'.concat(index);
@@ -212,13 +116,6 @@ function drawMasterChart(all_parabolas) {
     b.push((all_parabolas[index])[1]);
      }
 
-
-
-
-
-
-
-//  /*
     chart = c3.generate({
 
       bindto: '#chart_all',
@@ -228,12 +125,10 @@ function drawMasterChart(all_parabolas) {
 
             label: {
               text: 'Y axis',
-              position: 'outer-middle' },
-            //center: y_points[1],         
+              position: 'outer-middle' },        
             show: true,
             tick: {
                fit: true,
-               //values: y_points, 
               },
             padding : {
               top: 0,
@@ -242,17 +137,12 @@ function drawMasterChart(all_parabolas) {
            },
 
         x: {
-            //type: 'category',
             show: true,
             label: {
                 text: 'X axis',
                 position: 'outer-center' },
             tick: {
-              //culling: {
-                //max: 4 // the number of tick texts will be adjusted to less than this value
-              //},
               multiline: false,
-              //values: x_points,
               },
 
             }
@@ -262,15 +152,8 @@ function drawMasterChart(all_parabolas) {
 
         xs: a,
 
-        //x: 'data_x',
-        //y: 'data_y',
-        //parabola: 'parabola',
-        //x_data: 'data_x',
-        //y_data: 'data_y',
-
         columns: b,
         type: 'spline'
-
       },
 
       grid: {
@@ -288,5 +171,4 @@ function drawMasterChart(all_parabolas) {
     height: 1200,
     width: 1200
     });
-//  */
   };
